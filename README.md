0a5f7dd1ae7d345460b473f4bb93abf790a3c10e
Matthias Straka <mstraka84@gmail.com>
2020-05-26 13:18:03 +0200

    initial version for RAID1 systems

--- README.md
+++ README.md
@@ -7,5 +7,25 @@ This Python script can be used to collect data about BTRFS from a linux system b
 ## What data is collected
 TODO
 
+## Related information
+https://godoc.org/github.com/prometheus/procfs/btrfs
+
 # Usage
-TODO
+1. Copy `btrfs-collector.py` to a location accessible by telgraf. For example `/etc/telegraf/btrfs-collector.py`.
+2. Update `/etc/telegraf/telegraf.conf` to call the script:
+
+```
+    [[inputs.exec]]
+    commands = [
+        "python3 /etc/telegraf/btrfs-collector.py"
+        ]
+    timeout = "5s"
+    data_format = "influx"
+```
+3. Restart the telegraf service (e.g. `sudo systemctl restart telegraf.service`)
+
+# Requirements
+* python3
+* BTRFS filesystem
+* telegraf
+* InfluxDB
---
+++ btrfs-collector.py
@@ -0,0 +1,73 @@
+#!/bin/python3
+
+import os
+import re
+
+###############################################################################
+# Configuration
+###############################################################################
+# Base-dir for BTRFS information
+basedir = "/sys/fs/btrfs/"
+measurement = "btrfs"
+
+def isFilesystem(folder):
+    return re.fullmatch("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}", folder) is not None
+
+def readFile(path):
+    ''' Read a single line file '''
+    with open(path, "r") as f:
+        return f.read().rstrip('\n')
+
+def makeLineProtocol(measurement, tags, values):
+    ''' Convert to influx line protocol data line: measurement,tag1=name1,tag2=name2 field1=value1,field2=value2 ''' 
+    measurementid = measurement + ","
+    measurementid += ",".join(["{}={}".format(k, v) for k,v in tags.items()])
+    valuelist =      ",".join(["{}={}i".format(k, v) for k,v in values.items()])
+    return measurementid + " " + valuelist
+
+def emitLine(tagset, values):
+    print(makeLineProtocol(measurement, tagset, values))
+
+def processFilesystem(uuid):
+    path = os.path.join(basedir, uuid)
+
+    tagset = {"uuid": uuid}
+    tagset["label"] = readFile(os.path.join(path, "label"))
+
+    values = {}
+
+    # read global reserve
+    try:
+        tagset["profile"] = "single"
+        tagset["type"] = "globalreserve"
+        profilepath = os.path.join(path, "allocation")
+        values = {
+            "total_bytes": int(readFile(os.path.join(profilepath, "global_rsv_size")))
+        }
+        emitLine(tagset, values)
+    except:
+        pass
+
+    # read allocation data
+    for atype in ['data', 'metadata', 'system']:
+        try:
+            #TODO: detect data profile
+            tagset["profile"] = "raid1"
+            tagset["type"] = atype
+
+            profilepath = os.path.join(path, "allocation", atype)
+            values = {
+                "bytes_used":  int(readFile(os.path.join(profilepath, "bytes_used"))),
+                "total_bytes": int(readFile(os.path.join(profilepath, "total_bytes"))),
+                "disk_total":  int(readFile(os.path.join(profilepath, "disk_total"))),
+                "disk_used":   int(readFile(os.path.join(profilepath, "disk_used")))
+            }
+            emitLine(tagset, values)
+        except:
+            pass
+
+# find all available BTRFS filesystems
+for folder in os.listdir(basedir):
+    path = os.path.join(basedir, folder)
+    if isFilesystem(folder) and os.path.isdir(path):
+        processFilesystem(folder)
